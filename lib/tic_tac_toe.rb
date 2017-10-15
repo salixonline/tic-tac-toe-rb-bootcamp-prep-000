0b07182875c95303b4d1b4f25979b5290e630500
Armando <salixonline@gmail.com>
2017-10-15 22:33:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,7 +97,7 @@ def play(board)
     turn(board)
   end
   if won?(board)
-    player = current_player(board) == "X" ? "X" : ""
+    player = current_player(board) == "X" ? "X" : "X"
     puts "Congratulations #{player}!"
   elsif draw?(board)
     puts "Cat's Game!"
