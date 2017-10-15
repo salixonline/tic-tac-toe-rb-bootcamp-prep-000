736d91a05620ca8055d6218f59f462ed218a4c47
Armando <salixonline@gmail.com>
2017-10-15 21:51:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,7 +67,7 @@ def won?(board)
     pos_1 = board[win_index1]
     pos_2 = board[win_index2]
     pos_3 = board[win_index3]
-    if pos_1 == pos_2 && pos_2 == pos_3 && position_taken?(board[win_index1])
+    if pos_1 == pos_2 && pos_2 == pos_3 && position_taken?(board, win_index1)
       return win_combination
     end
   end
